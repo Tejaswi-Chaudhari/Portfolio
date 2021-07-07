e4df7a40166f00de34b16c82569f6dbe8ea8bb59
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-07-08 01:03:58 +0530

    Dark mode added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './components'
 
