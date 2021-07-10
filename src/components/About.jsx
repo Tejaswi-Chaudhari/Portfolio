b4ea58451f61c32576272cb3f56f7faf6cf1f54f
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-07-10 15:14:17 +0530

    about done-2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-import { AttentionSeeker } from "react-awesome-reveal";
-import './styles.css'
-import helloLight from '../media/hello-light.png'
-import helloDark from '../media/hello-dark.png'
+import { AttentionSeeker, Fade } from "react-awesome-reveal";
+import './styles.css';
+import helloLight from '../media/hello-light.png';
+import helloDark from '../media/hello-dark.png';
+import connectLight from '../media/connect-light.png';
+import connectDark from '../media/connect-dark.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 const About = ({darkMode}) => {
 
@@ -63,11 +73,10 @@ const About = ({darkMode}) => {
         document.body.appendChild(css);
     };
 
-
     return (
         <div>
             <AttentionSeeker shakeX>
-                <h1 className="display-3" align="center">HELLO!<code> I'm Tejaswi</code></h1>
+                <h1 className="display-3 hello" align="center">HELLO!<code> I'm Tejaswi</code></h1>
                 <h2 className="lead" align="center">I
                     <span
                         class="txt-rotate"
@@ -76,10 +85,54 @@ const About = ({darkMode}) => {
                     </span>
                 </h2>
                 <center>
-                <img src={darkMode? helloDark : helloLight} alt="hello" className="hello-img"/>
+                  <img src={darkMode? helloDark : helloLight} alt="hello" className="hello-img img-fluid animated"/>
                 </center>
             </AttentionSeeker>
+            <hr></hr>
+            <br></br>
+            <Fade>
+              <Container align='center'>
+                <Row>
+                  <Col lg={8} align='center'>
+                    <Card className="card">
+                      <Card.Body>
+                        <Card.Title>Let's have an introduction!</Card.Title>
+                        <Card.Text>
+                          <p>I am a passionate developer and believe in networking a lot. Many things excite me in this expanse of technology, and I like to explore and keep my options open all the time. I am up for challenges that take me by surprise and make my head hurt. I'm comfortable working on any domain if I find the project is worth it. You can imagine me focused on the laptop screen and typing or taking down notes. Apart from technical things, I enjoy good books and verses, and I have a good eye for art.</p>
+                          <p>Now that you know me a bit, please feel free to drop a text because Communication is a two-way street.</p>
+                        </Card.Text>
+                        <Button variant={darkMode ? "outline-light" : "outline-dark"} className='a'>
+                          <FontAwesomeIcon className="dark" size="lg" icon={faFile}/> View Resume
+                        </Button>
+                        <Button variant={darkMode ? "outline-light" : "outline-dark"} className='a'>
+                          <FontAwesomeIcon className="dark" size="lg" icon={faFile}/> Download Resume
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                  <Col lg={4} align='center'>
+                    <Card className="card">
+                      <Card.Body>
+                        <Card.Title>Let's Connect!</Card.Title>
+                        <a href="https://www.linkedin.com/in/tejaswi-chaudhari/" target="_blank" rel="noreferrer noopener">
+                          <Button variant={darkMode ? "outline-light" : "outline-dark"} className='a'>
+                            <FontAwesomeIcon className="dark" size="lg" icon={faLinkedin}/> Tejaswi Chaudhari
+                          </Button>
+                        </a>
+                        <a href="https://github.com/Tejaswi-Chaudhari" target="_blank" rel="noreferrer noopener">
+                          <Button variant={darkMode ? "outline-light" : "outline-dark"}>
+                            <FontAwesomeIcon className="dark" size="lg" icon={faGithub}/> Tejaswi-Chaudhari
+                          </Button>
+                        </a>
+                        <img src={darkMode? connectDark : connectLight} alt="connect" className="connect-img img-fluid animated"/>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+              </Container>
+            </Fade>
         </div>
+        
     )
 }
 
